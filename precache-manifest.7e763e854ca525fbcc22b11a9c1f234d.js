0513979279c366008a51b9cb4b0d53cd919d9f88
bloomy215 <bmbloomquist@gmail.com>
2019-04-26 22:24:31 -0500

    Updates

---
+++ precache-manifest.7e763e854ca525fbcc22b11a9c1f234d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f20f50ef74a9c5107f43121f20e5aaa",
+    "url": "/clicky-game/static/media/background.1f20f50e.jpg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "1cae6e627aa9bcda585e",
+    "url": "/clicky-game/static/js/main.8f951a89.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1cae6e627aa9bcda585e",
+    "url": "/clicky-game/static/css/main.874e79ce.chunk.css"
+  },
+  {
+    "revision": "aecaf8cc2833d36b685577ddb229d6e2",
+    "url": "/clicky-game/index.html"
+  }
+];
--- src/components/Container.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/Container.css";
-
-const Container = ( props ) => {
-  return ( <div className = "container-fluid animated zoomIn">{ props.children }</div> );
-}
-
-export default Container;
