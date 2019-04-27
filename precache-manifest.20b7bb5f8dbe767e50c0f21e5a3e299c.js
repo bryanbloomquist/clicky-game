0dcaaade924d03ddf98281cdccf30d54bda0bd2e
bloomy215 <bmbloomquist@gmail.com>
2019-04-27 10:53:01 -0500

    Updates

---
+++ precache-manifest.20b7bb5f8dbe767e50c0f21e5a3e299c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f20f50ef74a9c5107f43121f20e5aaa",
+    "url": "/clicky-game/static/media/background.1f20f50e.jpg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "e73a8c6d5098021d1923",
+    "url": "/clicky-game/static/js/main.6b04e6a4.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e73a8c6d5098021d1923",
+    "url": "/clicky-game/static/css/main.9243a844.chunk.css"
+  },
+  {
+    "revision": "5fb8aeb6a954693d96ff7af49e8054de",
+    "url": "/clicky-game/index.html"
+  }
+];
